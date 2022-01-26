9d9a2e51a771af5ec4189bcc0ac534afb8862fb8
naim edoo <naim.e2703@gmail.com>
2022-01-26 14:12:35 +0100

    exercice jour02

---
+++ javascript/jour02.js
@@ -0,0 +1,52 @@
+// 01 - Number
+
+let integer = 102;
+let float = 13.9;
+console.log(integer);
+console.log(float);
+ // 03- Round
+
+ let sum = 1.5;
+ let rounded = Math.round(sum);
+ console.log(rounded);
+
+ // 02 - Convert
+ let basic = 34;
+ let stringified = "34";
+ console.log(stringified);
+
+ // 04 - Arithmétique
+//   let test = 12;
+//   let bis = 5;
+//   console.log(test + bis);
+//   console.log(test - bis);
+//   console.log(test / bis);
+//   console.log(test * bis);
+
+  // 05 - Comparaison
+  let test = 143;
+  let bis = 219;
+  console.log(test > bis);
+  console.log(test < bis);
+  console.log(test >= bis);
+  console.log(test <= bis);
+  console.log(test == bis);
+  console.log(test === bis);
+  console.log(test != bis);
+  console.log(test !== bis);
+
+  // 06 - Condition
+
+  let limit = 50;
+  let score = 35;
+  if (score >= limit) {
+      console.log("ok good")
+  } else {
+      console.log("Oh nooo...")
+  }
+ 
+  // 07 - Condition II
+   let password = "azerty";
+   if (password.length > 5) {
+       console.log("The password is secure")
+   }
