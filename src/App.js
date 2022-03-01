1ce64e24ccc7bf751dc75b7d407e4f9934dabfa2
naim edoo <naim.e2703@gmail.com>
2022-03-01 14:05:12 +0100

    exercise elsy

--- src/App.js
+++ src/App.js
@@ -1,32 +1,79 @@
 import React from "react";
 import './App.css';
-import Box from "./Components/box";
-import'./Styles/Global.css';
+import './Styles/Global.css'
+import Box from './Components/box';
 
-const tempMin = -20;
+const tempMin = 20;
 const tempMax = 40;
 const heartMin = 80;
 const heartMax = 180;
 const stepsMin = 0;
 const stepsMax = 50000;
 
-function App() {
-    return (
-     <div className="container-fluid">
-        <div className="row">
-        {/* WATER */}
-        <Box icon = "local_drink" color="#3A85FF" value={1.5} unit ="L"/>
-        {/* Steps */}
-        <Box icon ="directions_walk" color="black" value={3000} unit ="steps"/>
-        {/*HEART*/}
-        <Box icon= "favorite" color="red" value={120} unit="bpm"/>
-        {/* TEMPERATURE */}
-        <Box icon="wb_sunny" color="yellow" value={-10} unit="°C"/>
-        <p>Heart : {heartMin}</p>
-        <p>Heart : {tempMin}</p>
-        <p>Heart : {stepsMin}</p>
-        </div>
-      </div>
-    );
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			water: 1.5,
+			heart: 120,
+			temperature: -10,
+			steps: 3000
+		}
+	}
+	calculateWater = () => {
+		let water = 1.5;
+		const temperature = this.state.temperature;
+		const heart = this.state.heart;
+		const steps = this.state.steps;
+		if (temperature > 20) {
+			water += temperature % 20 * 0.02;
+		}
+		if (heart > 120) {
+			water += heart % 120 * 0.00008;
+		}
+		if (steps > 10_000) {
+			water += steps % 10_000 * 0.00002;
+		}
+		water = water.toFixed(2)
+		this.setState({ water })
+	}
+
+	onHeartChange = (e) => {
+		let heart = e.target.value
+		this.setState({ heart });
+		this.calculateWater();
+	}
+	onTempChange = (e) => {
+		let temperature = e.target.value
+		this.setState({ temperature });
+		this.calculateWater();
+	}
+	onStepsChange = (e) => {
+		let Steps = e.target.value
+		this.setState({Steps});
+		this.calculateWater();
+	}
+
+	render() {
+		return (
+			<div className="container-fluid">
+				<div className="row">
+
+					{/* Water */}
+					<Box icon="local_drink" value={this.state.water} unit="L" color="#3A85FF" />
+
+					{/* Steps */}
+					<Box icon="directions_walk" min={stepsMin} max={stepsMax} value={this.state.steps} onChange={this.onStepsChange} unit="steps" color="black" />
+
+					{/* Heart */}
+					<Box icon="favorite" min={heartMin} max={heartMax} value={this.state.heart} onChange={this.onHeartChange} unit="bpm" color="red" />
+
+					{/* Temperature */}
+					<Box icon="wb_sunny" min={tempMin} max={tempMax} value={this.state.temperature} onChange={this.onTempChange} unit="°C" color="yellow" />
+				</div>
+			</div>
+		);
+	}
 }
 export default App;
--- src/Components/box.jsx
+++ src/Components/box.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
 
-export default function Box(props) {
+export default class box extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  renderInput() {
+    if (this.props.icon !== "local_drink") {
+      return <input type="range" min={this.props.min} max={this.props.max} value={this.props.value} onInput={this.props.onChange} />
+    } else {
+      return null
+    }
+  }
+  render() {
+    let stepRange = 1;
+    if (this.props.icon === "direction_walk") {
+       stepRange = 1_000
+    }
     return (
-       <div className='box col-sm-3 col-6'>
-        <span className ="material-icons" style={{fontSize : "100px",color:props.color}}>{props.icon}</span>
-      <p>{props.value} {props.unit}</p>
+     <div className="box col-sm-3 col-6">
+        <span className="material-icons" style={{ fontSize: "100px", color: this.props.color }}>
+          {this.props.icon}
+        </span>
+        <p>{this.props.value} {this.props.unit}</p>
+        {this.renderInput()}
       </div>
     )
+  }
 }
